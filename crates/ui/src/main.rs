1e983e80b67fabfa2b5778596628832cb373c798
Jen Tak <goodhoko@gmail.com>
2024-12-15 01:03:53 +0900

    Visualize computer by self-similarity matrix

--- crates/ui/src/main.rs
+++ crates/ui/src/main.rs
@@ -1,12 +1,17 @@
 use std::{
     ops::Deref,
     sync::{Arc, RwLock},
-    thread,
-    time::{Duration, Instant},
 };
 
-use audio_anemometer::{computer::Computer, simulator::Simulator};
+use audio_anemometer::{
+    computer::Computer,
+    simulator::{simulate_audio_pipeline, Simulator},
+};
 
+use wgpu::{
+    BindGroupDescriptor, BindGroupLayoutDescriptor, BindGroupLayoutEntry, Device, Extent3d,
+    Instance, PrimitiveTopology, ShaderStages, Texture, TextureView,
+};
 use winit::{
     event::{Event, WindowEvent},
     event_loop::EventLoop,
@@ -16,10 +21,10 @@ use winit::{
 const COMPARISON_WINDOW_WIDTH: usize = 1024;
 const MAX_EXPECTED_DELAY_SAMPLES: usize = 2048;
 
-// TODO: make these dynamically changeable by the user by winit key events
-const DELAY_SAMPLES: usize = 999;
-const ATTENUATION: f32 = 0.5;
-const SIGNAL_TO_NOISE_RATIO: f32 = 5.0;
+// TODO: make these dynamically changeable by winit key events.
+const DELAY_SAMPLES: usize = 333;
+const ATTENUATION: f32 = 1.0;
+const SIGNAL_TO_NOISE_RATIO: f32 = 10.0;
 
 fn main() {
     let simulator = Arc::new(RwLock::new(Simulator::new(
@@ -32,78 +37,25 @@ fn main() {
         COMPARISON_WINDOW_WIDTH,
     )));
 
-    spawn_audio_pipeline_simulation(&computer, &simulator);
-    // TODO: run GUI instead
-    // run_tui(computer);
-    run_gui(computer);
-}
+    simulate_audio_pipeline(&computer, &simulator);
 
-/// Spawn a thread that advances the simulator and the computer.
-fn spawn_audio_pipeline_simulation(
-    computer: &Arc<RwLock<Computer>>,
-    simulator: &Arc<RwLock<Simulator>>,
-) {
-    let computer = Arc::clone(computer);
-    let simulator = Arc::clone(simulator);
-
-    thread::spawn(move || {
-        let mut samples = 0;
-        let mut last_report = Instant::now();
-        loop {
-            let output_sample = computer.write().unwrap().output_sample();
-            let input_sample = simulator.write().unwrap().tick(output_sample);
-            computer.write().unwrap().record_sample(input_sample);
-
-            samples += 1;
-
-            if last_report.elapsed() > Duration::from_secs(1) {
-                println!("processed {samples} samples");
-                samples = 0;
-                last_report = Instant::now();
-            }
-        }
-    });
-}
-
-// TODO:
-// - init winit, wgpu, etc.
-// - start event loop
-// - on RedrawRequested
-//   - lock, clone, unlock the computer
-//   - compute delay
-//   - enqueue the computer's buffers, and the delay as uniforms to a render pass
-//   - run the render pass and present
-//   - ask for another redraw
-//
-// - on certain key press change the params of the simulator (delay, signal2noise ration, attenuation)
-fn run_gui(_computer: Arc<RwLock<Computer>>) {
     let event_loop = EventLoop::new().unwrap();
     let window = winit::window::WindowBuilder::new()
         .with_title("Audio-anemometer Visualization")
-        .with_inner_size(winit::dpi::LogicalSize::new(900, 900))
+        .with_inner_size(winit::dpi::LogicalSize::new(1600, 800))
         .build(&event_loop)
         .unwrap();
 
-    pollster::block_on(run(event_loop, window));
+    pollster::block_on(run(event_loop, window, computer));
 }
 
-async fn run(event_loop: EventLoop<()>, window: Window) {
+async fn run(event_loop: EventLoop<()>, window: Window, computer: Arc<RwLock<Computer>>) {
     let mut size = window.inner_size();
     size.width = size.width.max(1);
     size.height = size.height.max(1);
 
     let instance = wgpu::Instance::default();
-
-    let surface = instance.create_surface(&window).unwrap();
-    let adapter = instance
-        .request_adapter(&wgpu::RequestAdapterOptions {
-            power_preference: wgpu::PowerPreference::default(),
-            force_fallback_adapter: false,
-            // Request an adapter which can render to our surface
-            compatible_surface: Some(&surface),
-        })
-        .await
-        .expect("Failed to find an appropriate adapter");
+    let (surface, adapter) = get_surface_and_adapter(instance, &window).await;
 
     // Create the logical device and command queue
     let (device, queue) = adapter
@@ -111,7 +63,6 @@ async fn run(event_loop: EventLoop<()>, window: Window) {
             &wgpu::DeviceDescriptor {
                 label: None,
                 required_features: wgpu::Features::empty(),
-                // Make sure we use the texture resolution limits from the adapter, so we can support images the size of the swapchain.
                 required_limits: wgpu::Limits::downlevel_webgl2_defaults()
                     .using_resolution(adapter.limits()),
                 memory_hints: wgpu::MemoryHints::MemoryUsage,
@@ -121,16 +72,75 @@ async fn run(event_loop: EventLoop<()>, window: Window) {
         .await
         .expect("Failed to create device");
 
-    // Load the shaders from disk
-    let shader = device.create_shader_module(wgpu::include_wgsl!("shader.wgsl"));
+    let horizontal_size = MAX_EXPECTED_DELAY_SAMPLES + COMPARISON_WINDOW_WIDTH;
+    let (horizontal_texture, horizontal_texture_size, horizontal_texture_view) =
+        get_1d_texture_size_view(&device, horizontal_size);
+
+    let vertical_size = COMPARISON_WINDOW_WIDTH;
+    let (vertical_texture, vertical_texture_size, vertical_texture_view) =
+        get_1d_texture_size_view(&device, vertical_size);
+
+    let sampler = device.create_sampler(&Default::default());
+
+    let bind_group_layout = device.create_bind_group_layout(&BindGroupLayoutDescriptor {
+        label: None,
+        entries: &[
+            BindGroupLayoutEntry {
+                binding: 0,
+                visibility: ShaderStages::FRAGMENT,
+                ty: wgpu::BindingType::Texture {
+                    sample_type: wgpu::TextureSampleType::Float { filterable: false },
+                    view_dimension: wgpu::TextureViewDimension::D1,
+                    multisampled: false,
+                },
+                count: None,
+            },
+            BindGroupLayoutEntry {
+                binding: 1,
+                visibility: ShaderStages::FRAGMENT,
+                ty: wgpu::BindingType::Texture {
+                    sample_type: wgpu::TextureSampleType::Float { filterable: false },
+                    view_dimension: wgpu::TextureViewDimension::D1,
+                    multisampled: false,
+                },
+                count: None,
+            },
+            BindGroupLayoutEntry {
+                binding: 2,
+                visibility: ShaderStages::FRAGMENT,
+                ty: wgpu::BindingType::Sampler(wgpu::SamplerBindingType::NonFiltering),
+                count: None,
+            },
+        ],
+    });
+    let bind_group = device.create_bind_group(&BindGroupDescriptor {
+        label: None,
+        layout: &bind_group_layout,
+        entries: &[
+            wgpu::BindGroupEntry {
+                binding: 0,
+                resource: wgpu::BindingResource::TextureView(&horizontal_texture_view),
+            },
+            wgpu::BindGroupEntry {
+                binding: 1,
+                resource: wgpu::BindingResource::TextureView(&vertical_texture_view),
+            },
+            wgpu::BindGroupEntry {
+                binding: 2,
+                resource: wgpu::BindingResource::Sampler(&sampler),
+            },
+        ],
+    });
 
     let pipeline_layout = device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
         label: None,
-        bind_group_layouts: &[],
+        bind_group_layouts: &[&bind_group_layout],
         push_constant_ranges: &[],
     });
 
     let swapchain_format = surface.get_capabilities(&adapter).formats[0];
+
+    let shader = device.create_shader_module(wgpu::include_wgsl!("shader.wgsl"));
 
     let render_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
         label: None,
@@ -147,7 +157,10 @@ async fn run(event_loop: EventLoop<()>, window: Window) {
             compilation_options: Default::default(),
             targets: &[Some(swapchain_format.into())],
         }),
-        primitive: wgpu::PrimitiveState::default(),
+        primitive: wgpu::PrimitiveState {
+            topology: PrimitiveTopology::TriangleStrip,
+            ..Default::default()
+        },
         depth_stencil: None,
         multisample: wgpu::MultisampleState::default(),
         multiview: None,
@@ -164,7 +177,7 @@ async fn run(event_loop: EventLoop<()>, window: Window) {
         // Have the closure take ownership of the resources.
         // `event_loop.run` never returns, therefore we must do this to ensure
         // the resources are properly cleaned up.
-        let _ = (&instance, &adapter, &shader, &pipeline_layout);
+        let _ = (&adapter, &shader, &pipeline_layout);
 
         let Event::WindowEvent {
             window_id: _,
@@ -180,11 +193,59 @@ async fn run(event_loop: EventLoop<()>, window: Window) {
                 config.width = new_size.width.max(1);
                 config.height = new_size.height.max(1);
                 surface.configure(&device, &config);
-                // On macos the window needs to be redrawn manually after resizing
-                window.request_redraw();
             }
             WindowEvent::RedrawRequested => {
-                let frame = surface
+                let computer = computer.read().unwrap().deref().clone();
+                let hor_samples = computer
+                    .output_buffer()
+                    .iter()
+                    .flat_map(|item| item.to_le_bytes())
+                    .collect::<Vec<_>>();
+                let ver_samples = computer
+                    .input_buffer()
+                    .iter()
+                    .flat_map(|item| item.to_le_bytes())
+                    .collect::<Vec<_>>();
+
+                queue.write_texture(
+                    // Tells wgpu where to copy the data
+                    wgpu::ImageCopyTexture {
+                        texture: &horizontal_texture,
+                        mip_level: 0,
+                        origin: wgpu::Origin3d::ZERO,
+                        aspect: wgpu::TextureAspect::All,
+                    },
+                    // The actual data
+                    &hor_samples,
+                    // The layout of the texture
+                    wgpu::ImageDataLayout {
+                        offset: 0,
+                        bytes_per_row: Some(hor_samples.len() as u32),
+                        rows_per_image: Some(1),
+                    },
+                    horizontal_texture_size,
+                );
+
+                queue.write_texture(
+                    // Tells wgpu where to copy the data
+                    wgpu::ImageCopyTexture {
+                        texture: &vertical_texture,
+                        mip_level: 0,
+                        origin: wgpu::Origin3d::ZERO,
+                        aspect: wgpu::TextureAspect::All,
+                    },
+                    // The actual data
+                    &ver_samples,
+                    // The layout of the texture
+                    wgpu::ImageDataLayout {
+                        offset: 0,
+                        bytes_per_row: Some(ver_samples.len() as u32),
+                        rows_per_image: Some(1),
+                    },
+                    vertical_texture_size,
+                );
+
+                let frame: wgpu::SurfaceTexture = surface
                     .get_current_texture()
                     .expect("Failed to acquire next swap chain texture");
                 let view = frame
@@ -207,11 +268,14 @@ async fn run(event_loop: EventLoop<()>, window: Window) {
                     occlusion_query_set: None,
                 });
                 render_pass.set_pipeline(&render_pipeline);
-                render_pass.draw(0..3, 0..1);
+                render_pass.set_bind_group(0, &bind_group, &[]);
+                render_pass.draw(0..4, 0..1);
                 drop(render_pass);
 
                 queue.submit(Some(encoder.finish()));
                 frame.present();
+
+                window.request_redraw();
             }
             WindowEvent::CloseRequested => target.exit(),
             _ => {}
@@ -221,31 +285,43 @@ async fn run(event_loop: EventLoop<()>, window: Window) {
     res.unwrap();
 }
 
-#[expect(unused)]
-fn run_tui(computer: Arc<RwLock<Computer>>) {
-    let mut accumulated_delay = 0;
-    let mut delays = 0;
-    let mut last_report = Instant::now();
-    loop {
-        // Computing the delay() is much more expensive than cloning the entire computer.
-        // To lower lock contention, copy a snapshot of the computer to this thread
-        // and immediately release the lock.
-        let computer = computer.read().unwrap().deref().clone();
+async fn get_surface_and_adapter(
+    instance: Instance,
+    window: &Window,
+) -> (wgpu::Surface, wgpu::Adapter) {
+    let surface = instance.create_surface(window).unwrap();
+    let adapter = instance
+        .request_adapter(&wgpu::RequestAdapterOptions {
+            power_preference: wgpu::PowerPreference::default(),
+            force_fallback_adapter: false,
+            // Request an adapter which can render to our surface
+            compatible_surface: Some(&surface),
+        })
+        .await
+        .expect("Failed to find an appropriate adapter");
 
-        if let Some(delay) = computer.delay() {
-            accumulated_delay += delay;
-            delays += 1;
+    (surface, adapter)
+}
 
-            if last_report.elapsed() > Duration::from_secs(1) {
-                let avg = accumulated_delay as f32 / delays as f32;
-                println!("delay {avg} samples (averaged over {delays} computations)");
-                accumulated_delay = 0;
-                delays = 0;
-                last_report = Instant::now();
-            }
-        } else {
-            // The computer is not ready yet. Give it some time to accumulate more samples.
-            thread::sleep(Duration::from_millis(100));
-        }
-    }
+fn get_1d_texture_size_view(device: &Device, size: usize) -> (Texture, Extent3d, TextureView) {
+    let texture_size = wgpu::Extent3d {
+        width: size as u32,
+        height: 1,
+        depth_or_array_layers: 1,
+    };
+    let texture = device.create_texture(&wgpu::TextureDescriptor {
+        size: texture_size,
+        mip_level_count: 1,
+        sample_count: 1,
+        dimension: wgpu::TextureDimension::D1,
+        format: wgpu::TextureFormat::R32Float,
+        // TEXTURE_BINDING tells wgpu that we want to use this texture in shaders
+        // COPY_DST means that we want to copy data to this texture
+        usage: wgpu::TextureUsages::TEXTURE_BINDING | wgpu::TextureUsages::COPY_DST,
+        label: Some("horizontal texture"),
+        view_formats: &[],
+    });
+    let view = texture.create_view(&wgpu::TextureViewDescriptor::default());
+
+    (texture, texture_size, view)
 }
--- crates/ui/src/shader.wgsl
+++ crates/ui/src/shader.wgsl
@@ -1,11 +1,46 @@
-@vertex
-fn vs_main(@builtin(vertex_index) in_vertex_index: u32) -> @builtin(position) vec4<f32> {
-    let x = f32(i32(in_vertex_index) - 1);
-    let y = f32(i32(in_vertex_index & 1u) * 2 - 1);
-    return vec4<f32>(x, y, 0.0, 1.0);
+const vertices = array(
+    vec2(-1., -1.),
+    vec2(-1., 1.),
+    vec2(1., -1.),
+    vec2(1., 1.),
+);
+
+const tex_coords_arr = array(
+    vec2(0., 0.),
+    vec2(0., 1.),
+    vec2(1., 0.),
+    vec2(1., 1.),
+);
+
+
+struct VertexOutput {
+    @builtin(position) clip_position: vec4<f32>,
+    @location(0) tex_coords: vec2<f32>,
 }
 
+@vertex
+fn vs_main(@builtin(vertex_index) in_vertex_index: u32) -> VertexOutput {
+    var out: VertexOutput;
+    let coordinates = vertices[in_vertex_index];
+    out.tex_coords = tex_coords_arr[in_vertex_index];
+    out.clip_position = vec4<f32>(coordinates, 0.0, 1.0);
+    return out;
+} 
+
+@group(0) @binding(0)
+var hor_text: texture_1d<f32>;
+@group(0) @binding(1)
+var ver_text: texture_1d<f32>;
+@group(0) @binding(2)
+var s_diffuse: sampler;
+
+
 @fragment
-fn fs_main() -> @location(0) vec4<f32> {
-    return vec4<f32>(1.0, 0.0, 0.0, 1.0);
+fn fs_main(in: VertexOutput) -> @location(0) vec4<f32> {
+    let hor = textureSample(hor_text, s_diffuse, in.tex_coords.x).r;
+    let ver = textureSample(ver_text, s_diffuse, in.tex_coords.y).r;
+    let difference = abs(ver - hor);
+
+    return vec4(vec3(-log(difference) / 10.), 0.);
 }
+ 
